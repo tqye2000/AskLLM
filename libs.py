7df56b793d0a012920a89b5e1e92d05eabcd8fec
Tian-Qing Ye <tian-qing.ye@siemens-healthineers.com>
2024-04-12 17:42:13 +0100

    further updated

--- libs.py
+++ libs.py
@@ -223,3 +223,19 @@ def GetContexts(uploaded_file):
 
     return Content
 
+#@st.cache_data()
+#def get_remote_ip() -> str:
+#    """Get remote ip."""
+
+#    try:
+#        ctx = get_script_run_ctx()
+#        if ctx is None:
+#            return None
+
+#        session_info = runtime.get_instance().get_client(ctx.session_id)
+#        if session_info is None:
+#            return None
+#    except Exception as e:
+#        return None
+
+#    return session_info.request.remote_ip
